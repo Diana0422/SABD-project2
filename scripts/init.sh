811de6a4984f31d1e63f1de6c960d345beb226da
Giacomo <giacomo.redjack@gmail.com>
2022-07-13 21:09:01 +0200

    Presentation

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,3 @@
 dir=$(pwd)
-### Compiling the kafka program
-#mvn -pl sabd2-kafka package
-### Compiling the flink program
-#mvn -pl sabd2-flink package
-# Creating Consumer and Producer docker images
 mkdir -p "$dir"/sabd2-kafka/consumer/target/
 cp "$dir"/sabd2-kafka/target/sabd2-kafka-1.0-jar-with-dependencies.jar "$dir"/sabd2-kafka/consumer/target/
